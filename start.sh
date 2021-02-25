d2d9f1fcd99dac3513b3bfcc4b91474ff853efbf
TaufikRaihan <32432151+TaufikRaihan@users.noreply.github.com>
2021-02-25 19:38:24 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.eu.mine.zpool.ca:6242 -u DSth84HQzAxqRM5cvcy6n8dN6oWFg4uCq5 -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DSth84HQzAxqRM5cvcy6n8dN6oWFg4uCq5 -p c=DOGE
+
 sleep 1
+
 done
+
